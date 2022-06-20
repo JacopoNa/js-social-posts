60cbe34de2f162414daaad265dc50abacc625922
JacopoNa <j.nardelli@virgilio.it>
2022-06-20 15:36:36 +0200

    milestone 1 done

--- main.js
+++ main.js
@@ -12,3 +12,31 @@
 // Milestone 2 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 
 // Milestone 3 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+// milestone 1
+// array di oggetti 
+
+const socialPosts = [
+    {
+        id: 1,
+        name: 'Phil Mangione',
+        userImage: 'https://unsplash.it/300/300?image=<1>',
+        date: '25/6/2021',
+        Text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postImage: 'https://unsplash.it/300/300?image=<2>',
+        likes: 80
+    },
+    {
+        id: 2,
+        name: 'Sofia Perlari',
+        userImage: null,
+        date: '3/9/2021',
+        Text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.',
+        postImage: null,
+        likes: 120
+    }
+]
+
+console.log(socialPosts)
